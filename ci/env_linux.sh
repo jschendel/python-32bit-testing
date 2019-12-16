d4861961cb7907153600dd83c3d09634b822355a
Jeremy Schendel <jschendel@users.noreply.github.com>
2019-12-15 17:50:07 -0700

    add linux and python file

---
+++ ci/env_linux.sh
@@ -0,0 +1,45 @@
+#!/bin/bash -e
+
+MINICONDA_DIR="$HOME/miniconda3"
+
+if [ -d "$MINICONDA_DIR" ]; then
+    echo
+    echo "rm -rf "$MINICONDA_DIR""
+    rm -rf "$MINICONDA_DIR"
+fi
+
+echo "Install Miniconda"
+wget -q "https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh" -O miniconda.sh
+chmod +x miniconda.sh
+./miniconda.sh -b
+
+export PATH=$MINICONDA_DIR/bin:$PATH
+
+echo
+echo "which conda"
+which conda
+
+echo
+echo "update conda"
+conda config --set ssl_verify false
+conda config --set quiet true --set always_yes true --set changeps1 false
+conda install pip  # create conda to create a historical artifact for pip & setuptools
+conda update -n base conda
+
+echo "conda info -a"
+conda info -a
+
+echo "source deactivate"
+source deactivate
+
+echo "conda list (root environment)"
+conda list
+
+# Clean up any left-over from a previous build
+# (note workaround for https://github.com/conda/conda/issues/2679:
+#  `conda env remove` issue)
+conda remove --all -q -y -n test-env
+
+echo
+echo "conda create -q --name test-env python=${PYTHON_VERSION}"
+conda create --name test-env python=${PYTHON_VERSION}
---
+++ py32.py
@@ -0,0 +1,2 @@
+for x in range(2**61 - 2, 2**61 + 2):
+    print(x, hash(x))
