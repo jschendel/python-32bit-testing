b49112c520c3ed99ad1682024a311a3802543156
Jeremy Schendel <jschendel@users.noreply.github.com>
2019-12-15 21:36:34 -0700

    more or less have it!

--- ci/run_script.sh
+++ ci/run_script.sh
@@ -1,10 +1,4 @@
 #!/bin/bash -e
 
-if [ "$(uname)" == "Windows" ]; then
-    echo "setting windows path"
-    pydir="$PWD/pyinstall/${PYTHON_PKG}"
-    export PATH="${pydir}/tools:${pydir}/tools/scripts:$PATH"
-fi
-
 python python_info.py
 python python_script.py
