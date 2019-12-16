b171b73c9301d5481d1efe8e935522a44ccfc3f6
Jeremy Schendel <jschendel@users.noreply.github.com>
2019-12-15 19:40:35 -0700

    maybe simplify

--- ci/all_windows.sh
+++ ci/all_windows.sh
@@ -1,8 +1,8 @@
 # By default temp and cache directories are on C:\. Fix that.
-export TEMP="${AGENT_TEMPDIRECTORY}"
-export TMP="${AGENT_TEMPDIRECTORY}"
-export TMPDIR="${AGENT_TEMPDIRECTORY}"
-export PIP_CACHE_DIR="${AGENT_TEMPDIRECTORY}\\pip-cache"
+# export TEMP="${AGENT_TEMPDIRECTORY}"
+# export TMP="${AGENT_TEMPDIRECTORY}"
+# export TMPDIR="${AGENT_TEMPDIRECTORY}"
+# export PIP_CACHE_DIR="${AGENT_TEMPDIRECTORY}\\pip-cache"
 
 # Download and install Python from scratch onto D:\, instead of using the
 # pre-installed versions that azure pipelines provides on C:\.
@@ -10,7 +10,8 @@ export PIP_CACHE_DIR="${AGENT_TEMPDIRECTORY}\\pip-cache"
 nuget install "${PYTHON_PKG}" -Version "${PYTHON_VERSION}" \
 -OutputDirectory "$PWD/pyinstall" -ExcludeVersion \
 -Source "https://api.nuget.org/v3/index.json" \
--Verbosity detailed -DirectDownload -NonInteractive
+-Verbosity detailed -NonInteractive
+# -Verbosity detailed -DirectDownload -NonInteractive
 
 pydir="$PWD/pyinstall/${PYTHON_PKG}"
 export PATH="${pydir}/tools:${pydir}/tools/scripts:$PATH"
